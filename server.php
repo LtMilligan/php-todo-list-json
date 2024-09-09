3a6490284bcaea2d241c5da609b3a4e4c5cee23c
LtMilligan <gioele.miscia@gmail.com>
2024-09-09 22:56:16 +0200

    [UPD] Adding VUE in script.js for data, methods and mounted.

--- server.php
+++ server.php
@@ -1,3 +1,8 @@
 <?php
+    header('Content-type: application/json');
+    $data = file_get_contents('todo-list.json');
+    $dataArray = json_decode($data, true);
+    $dataJson = json_encode($dataArray);
 
+    echo $dataJson
 ?>
